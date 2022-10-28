13125b35e310b899f8c3638b42b918f363f62e6b
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-29 00:24:46 +0530

    build: update 3 files and create 1 file

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -21,15 +21,35 @@ export default function PostPage({ post }: { post: MDXPost }) {
             <Head>
                 <title>{post.meta.title}</title>
             </Head>
-            <main>
+            <main className="bg-black">
                 <div className="py-20 mx-auto lg:max-w-[65%] xs:max-w-[75%]">
                     <div>
-                        <Image src={post.meta.blogImage} alt="blog image" width="1920" height="1080" className="bg-black rounded-lg" />
-                    </div>
-                    <div className="">
-                        <article>
-                            <MDXRemote {...post.source} components={{ YouTube, Image }} />
-                        </article>
+                        <div>
+                            <Image src={post.meta.blogImage} alt="blog image" width="1920" height="1080" className="bg-black rounded-lg" />
+                        </div>
+                        <h1 className="text-7xl font-bold text-white">{post.meta.title}</h1>
+                        <div className="py-10">
+                            <div className="flex flex-row gap-2 justify-center text-start items-center ">
+                                <div>
+                                    <Image
+                                        src={post.meta.authorImage}
+                                        alt={post.meta.author}
+                                        width={50}
+                                        height={50}
+                                        className=" rounded-full"
+                                    />
+                                </div>
+                                <div>
+                                    <p className="text-white">{post.meta.author}</p>
+                                    <p className="text-white">{post.meta.date}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="">
+                            <article>
+                                <MDXRemote {...post.source} components={{ YouTube, Image }} />
+                            </article>
+                        </div>
                     </div>
                 </div>
             </main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,27 @@ import Community from '../layout/Community';
 import Header from '../layout/Header';
 import Team from '../layout/Team';
 
+import CommunityData from '../content/community.json';
+import EventData from '../content/events.json';
+import TeamData from '../content/team.json';
+
 export async function getStaticProps() {
 
-  const team_data = await fetch("http://localhost:3000/api/team");
-  const event_data = await fetch("http://localhost:3000/api/event");
-  const community_data = await fetch("http://localhost:3000/api/community");
-  const Team = await team_data.json()
-  const Event = await event_data.json()
-  const Community = await community_data.json()
+  // const team_data = await fetch("http://localhost:3000/api/team");
+  // const event_data = await fetch("http://localhost:3000/api/event");
+  // const community_data = await fetch("http://localhost:3000/api/community");
+  // const Team = await team_data.json()
+  // const Event = await event_data.json()
+  // const Community = await community_data.json()
   // sort the event by date
   // const sortedEvent = Event.sort((a, b) => {
   //   return (b.id) - (a.id);
   // });
+
+  const Team = TeamData;
+  const Event = EventData;
+  const Community = CommunityData;
+
 
   return {
     props: {
