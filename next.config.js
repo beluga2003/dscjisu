df23bbba973924e8ce21e383b5b4430c64290521
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-28 23:41:57 +0530

    build: fix

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
   images: {
     domains: ['i.pravatar.cc', 'res.cloudinary.com', "avatars.githubusercontent.com", "github.com", "upload.wikimedia.org", "e7.pngegg.com", "play-lh.googleusercontent.com"],
   },
-  i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en',
-    // localeDetection: false,
-  },
 };
 
 module.exports = nextConfig;
