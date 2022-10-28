df9b9b7facec32ce3c8d0a49473f3a8a9db9b45f
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-28 22:19:24 +0530

    perf: update pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,43 @@
-import PrimaryLayout from '../components/layouts/PrimaryLayout/Layout';
-import CommunityPartner from '../components/sections/communitypartner/community';
-import { NextPageWithLayout } from './page';
+// import SpeedDail from '../components/speedDail';
+import Chapter from '../layout/Chapter';
+import Community from '../layout/Community';
+import Events from '../layout/Events';
+import Header from '../layout/Header';
+import Team from '../layout/Team';
 
+export async function getStaticProps() {
 
-const Home: NextPageWithLayout = () => {
+  const team_data = await fetch("http://localhost:3000/api/team");
+  const event_data = await fetch("http://localhost:3000/api/event");
+  const community_data = await fetch("http://localhost:3000/api/community");
+  const Team = await team_data.json()
+  const Event = await event_data.json()
+  const Community = await community_data.json()
+  // sort the event by date
+  const sortedEvent = Event.sort((a, b) => {
+    return (b.id) - (a.id);
+  });
+
+  return {
+    props: {
+      team: Team,
+      event: sortedEvent,
+      community: Community
+    }, // will be passed to the page component as props
+  }
+}
+
+const Home = ({ team, event, community }) => {
   return (
     <>
-      <CommunityPartner />
+      <Header />
+      <Events event={event} />
+      <Team team={team} />
+      <Chapter />
+      <Community community={community} />
+      {/* <SpeedDail /> */}
     </>
   );
 };
 
 export default Home;
-
-Home.getLayout = (page) => {
-  return <PrimaryLayout>{page}</PrimaryLayout>;
-};
