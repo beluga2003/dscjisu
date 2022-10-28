60aa40c0ffb71cbf541aa01cfde143b665381214
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-28 22:21:12 +0530

    perf: update event.tsx

--- pages/event.tsx
+++ pages/event.tsx
@@ -29,7 +29,7 @@ export const getStaticProps: GetStaticProps = async () => {
     // console.log(res)
     return {
         props: {
-            events: res.data
+            events: res
         }
     }
 }
