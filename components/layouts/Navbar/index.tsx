0c5026085ddfbe41af8080f8e9783da14c6d94cb
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-29 13:05:44 +0530

    build: update components/layouts/Navbar/index.tsx

--- components/layouts/Navbar/index.tsx
+++ components/layouts/Navbar/index.tsx
@@ -18,25 +18,25 @@ function Navbar() {
 
     return (
         <>
-            <div className='lg:flex   flex-row justify-between  lg:items-center lg:justify-between px-5 py-2  shadow-sm bg-black bg-opacity-90  shadow-slate-50  sticky top-0  bg-bg-dark text-white z-50 '>
+            <div className='lg:flex   flex-row justify-between  items-center  px-5 py-2  shadow-sm bg-black bg-opacity-90  shadow-slate-50  sticky top-0  bg-bg-dark text-white z-50 '>
                 <div className="flex flex-row justify-between">
                     <div className='inline '>
                         <Link href='/' passHref>
-                            <div className=' flex gap-2 justify-center items-center'>
-                                <Image src="/images/gdsclogo.jpg" alt='gdsc logo' width={50} height={50} />
+                            <div className=' flex  justify-center items-center'>
+                                <Image src="/images/gdsc_nav_logo.png" alt='gdsc logo' width={50} height={50} />
                                 <span id="jis--university" className='text-black  font-mono font-semibold text-3xl  dark:text-white'>JIS University</span>
                             </div>
                         </Link>
                     </div>
-                    <div className="">
+                    <div className="flex justify-center items-center">
                         <span className=' lg:hidden text-3xl'>
                             {open ? <CloseIcon onClick={toogleMenu} className='cursor-pointer menu-options' /> : <SegmentIcon onClick={toogleMenu} className='cursor-pointer menu-options text-6xl' />}
                         </span>
                     </div>
                 </div>
 
-                <div className="links bg-dark  lg:flex flex-row hidden" id="menu-list">
-                    <ul className='lg:flex lg:flex-row lg:justify-end lg:opacity-100 opacity-100 text-center flex-col   p-10 gap-5 sm:flex   lg:py-0  lg:pl-0 '>
+                <div className="links bg-dark  lg:flex flex-row hidden justify-center items-center" id="menu-list">
+                    <ul className='flex lg:flex-row lg:justify-end lg:opacity-100 opacity-100 text-center items-center gap-10 flex-col lg:min-h-fit min-h-screen  p-10 sm:flex   lg:py-0  lg:pl-0 '>
                         <li onClick={toogleMenu}><Link href={'/'}  ><a href="#" className="nav-list   hover:font-semibold " >Home</a></Link></li>
                         <li onClick={toogleMenu}><Link href={'/event'} ><a href="#" className="nav-list   hover:font-semibold ">Events</a></Link></li>
                         <li onClick={toogleMenu}><Link href={'/blog'} ><a href="#" className="nav-list   hover:font-semibold ">Blogs</a></Link></li>
