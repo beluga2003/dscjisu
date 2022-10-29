abf8f0b65c2b71ca62675c16b99114ca077a27f2
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-10-29 10:57:12 +0530

    build: update BlogCard.mocks.ts

--- components/cards/blog/BlogCard.mocks.ts
+++ components/cards/blog/BlogCard.mocks.ts
@@ -8,7 +8,8 @@ const base: IBlogCard = {
   networks, and data from malicious attacks which is conducted by hackers.
   It&#39;s also known as information technology security or Ethical hacking.`,
   author: 'Rahul Kamilya',
-  time: '2 min read',
+  date: '2021-01-01',
+  readingTime: '2 min read',
   blogImage: '/blogs/why-cybersecurity-Important-!.jpg',
   authorImage: 'https://lh4.googleusercontent.com/o1ziIEHPfTEbTsioQWFGtFzrQftroni_oEk8h9MuZj5q88RJ6NZHYZIm4t8ByzQOM2c6sXevA-0nK9yrUitjky5hBM1TLiuc5lPsbtTzlN03ufSayK54Y2ose0Jonq9PgwgrDaIN8toDWJfe1H3sqNSh5ISEHeksST3zu2zLwR211o76MRusDlZ4',
   blogslug: 'why-cybersecurity-Important-!'
