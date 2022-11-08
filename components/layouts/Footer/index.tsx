fa400bda5ae68a6907dbfd2b231dfcad7b7161dd
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-11-08 11:50:07 +0530

    feat: update components/layouts/Footer/index.tsx

--- components/layouts/Footer/index.tsx
+++ components/layouts/Footer/index.tsx
@@ -2,12 +2,10 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import FollowTheSignsIcon from '@mui/icons-material/FollowTheSigns';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import RssFeedIcon from '@mui/icons-material/RssFeed';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import YouTubeIcon from '@mui/icons-material/YouTube';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -18,10 +16,10 @@ function Footer() {
     return (
         <div className=' bg-slate-900  '>
             <div className="footer">
-                <div className='grid  grid-cols-3  lg:gap-2 gap-4 text-white  p-10 text-start  lg:place-items-center  '>
+                <div className='grid  grid-cols-3  lg:gap-2 gap-4 text-white  p-10  lg:place-items-center '>
                     <div className='sm:col-span-1 col-span-3'>
-                        <Image src="/animation_gdsc.webp" height="80" width="90" alt='Google developer Student Club' className='h-[90px]' /> <br />
-                        <span className='text-white  font-mono font-medium'>Google Developer Student Club <br /> JIS University</span>
+                        <Image src="/images/gdsc_nav_logo.png" height="80" width="90" alt='Google developer Student Club' className='h-[90px]' /> <br />
+                        <span className='text-white  font-mono font-medium'>Google Developer Student Club <br /> JIS University Kolkata</span>
                         <div className='mt-5'>
                             <ul>
                                 <li ><Link href={'/'}  ><a href="#" className="font-mono font-light cursor-default hover:text-slate-200 text-white   hover:font-semibold decoration-red-600" >Home</a></Link></li>
@@ -40,25 +38,43 @@ function Footer() {
                         <div className='mt-5'>
                             <ul className=' text-white font-mono font-medium'>
                                 <li> <a href="https://www.facebook.com/groups/gdscjisu"><FacebookIcon className='text-white m-2' />@gdscjisu</a></li>
-                                <li> <a href="https://www.instagram.com/gdsc_jisu/"><InstagramIcon className='text-white m-2' />@gdsc_jisu</a></li>
                                 <li>  <a href="https://twitter.com/gdsc_jisu"><TwitterIcon className='text-white m-2' /></a>@gdsc_jisu</li>
                                 <li>  <a href="https://github.com/gdsc-jisu"><GitHubIcon className='text-white m-2' />@gdsc-jisu</a></li>
                                 <li> <a href="https://www.linkedin.com/company/gdsc-jisu"><LinkedInIcon className='text-white m-2' />@gdsc-jisu</a></li>
-                                <li>  <a href="https://www.youtube.com/"><YouTubeIcon className='text-white m-2' /></a>@404NotFound</li>
                             </ul>
                         </div>
                     </div>
 
 
 
-                    <div className='sm:col-span-1 col-span-3'>
+                    <div className='sm:col-span-1 col-span-3 content-start'>
                         <span className='text-white  font-mono font-bold text-xl underline underline-offset-4'>Resources</span>
                         <div className='mt-5'>
                             <ul className=' text-white font-mono font-medium'>
-                                <li> <a href="https://www.facebook.com/groups/gdscjisu"><InsertDriveFileIcon className='text-white m-2' />CheatSheets</a></li>
-                                <li> <a href="https://www.instagram.com/gdsc_jisu/"><RssFeedIcon className='text-white m-2' />Blogs</a></li>
-                                <li>  <a href="https://twitter.com/gdsc_jisu"><PlayCircleIcon className='text-white m-2' /></a>Videos</li>
-                                <li>  <a href="https://twitter.com/gdsc_jisu"><FollowTheSignsIcon className='text-white m-2' /></a>RoadMaps</li>
+                                <li> <Link passHref href="/cheatsheet">
+                                    <a>
+                                        <InsertDriveFileIcon className='text-white m-2' />CheatSheets
+                                    </a>
+                                </Link>
+                                </li>
+                                <li> <Link passHref href="/blog">
+                                    <a>
+                                        <RssFeedIcon className='text-white m-2' />Blogs
+                                    </a>
+                                </Link>
+                                </li>
+                                <li> <Link passHref href="/roadmap">
+                                    <a>
+                                        <FollowTheSignsIcon className='text-white m-2' />Roadmap
+                                    </a>
+                                </Link>
+                                </li>
+                                <li> <Link passHref href="/video">
+                                    <a>
+                                        <PlayCircleIcon className='text-white m-2' />Video
+                                    </a>
+                                </Link>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -74,7 +90,7 @@ function Footer() {
 
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
