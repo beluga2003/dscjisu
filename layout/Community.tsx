71d3fecba28677b939a35a43b6f0e37b42cf8887
Abbhiishek <abhishekkushwaha1479@gmail.com>
2022-11-08 11:57:34 +0530

    fix: ts

--- layout/Community.tsx
+++ layout/Community.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image"
-export default function Community({ community }) {
+import Image from "next/image";
+
+
+interface community {
+    community: CommunityProps[]
+}
+interface CommunityProps {
+
+    id: string;
+    name: string;
+    image: string;
+    description: string;
+    link: string;
+}
+
+export default function Community({ community }: community) {
     return (
         <div className='lg:p-20 sm:p-2 bg-white flex  flex-col text-center dark:bg-[#182232] py-10'>
             <h1 className='lg:text-6xl text-3xl dark:text-white font-mono'><span className='underline decoration-solid underline-offset-2 decoration-cyan-300 font-mono community'>Community</span> Partner</h1>
@@ -8,7 +22,7 @@ export default function Community({ community }) {
             </p>
             <div className='grid  lg:grid-flow-col  grid-flow-row   justify-center place-items-center p-10 gap-5'>
                 {
-                    community?.map((partner) => {
+                    community?.map((partner: CommunityProps) => {
                         return (
                             <div className='dark:bg-slate-600 bg-slate-100 rounded-lg p-10   transition-all duration-1000' key={partner.id} title={partner.description}>
                                 <Image src={partner.image} alt={partner.name} className='rounded-xl  h-20  transition-all duration-1000' height={100} width={100} />
